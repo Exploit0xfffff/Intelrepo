e21c150507887cc56f8b768409fa3ce0b20a687a
swayampakula.v.s.s.pavanakasinadha sarma <81065703+kasinadhsarma@users.noreply.github.com>
2023-06-06 17:47:27 +0530

    Update object.py

--- src/desktop/object.py
+++ src/desktop/object.py
@@ -2,7 +2,6 @@ import cv2
 import torch
 import torchvision
 from torchvision import models
-import numpy as np
 
 def object():
     # Check if GPU is available
@@ -13,45 +12,14 @@ def object():
     model = model.to(device)
     model.eval()
 
-    # Load class labels for object detection
-    class_labels = [
-    'background', 'person', 'bicycle', 'car', 'motorcycle',
-    'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
-    'fire hydrant', 'stop sign', 'parking meter', 'bench',
-    'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant',
-    'bear', 'zebra', 'giraffe', 'backpack', 'umbrella',
-    'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
-    'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
-    'skateboard', 'surfboard', 'tennis racket', 'bottle',
-    'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
-    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot',
-    'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
-    'potted plant', 'bed', 'dining table',
-    'toilet', 'tv', 'laptop', 'mouse', 'remote',
-    'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
-    'sink', 'refrigerator', 'book', 'clock', 'vase',
-    'scissors', 'teddy bear', 'hair drier', 'toothbrush',
-    'rickshaw', 'motor-rickshaw',
-    'traffic cone', 'street sign', 'road barrier', 'wheelbarrow',
-    'shopping cart', 'baby stroller', 'tractor', 'helicopter',
-    'jet ski', 'airship', 'aircraft carrier', 'hoverboard',
-    'scooter', 'tricycle', 'crane', 'bulldozer', 'excavator',
-    'forklift', 'golf cart', 'trolley', 'wagon', 'binoculars',
-    'umbrella', 'walking stick', 'crutch', 'fishing rod',
-    'hammock', 'tent', 'camping stove', 'campfire', 'marshmallow',
-    'flashlight', 'compass', 'map', 'backpack', 'suitcase',
-    'briefcase', 'wallet', 'purse', 'hand fan', 'umbrella hat',
-    'sun hat', 'fedora', 'top hat', 'bicycle helmet',
-    'hard hat', 'swim cap', 'bandana', 'headband', 'sunglasses',
-    't-shirt', 'pants', 'dress', 'shirt', 'jacket', 'coat',
-    'sweater', 'hoodie', 'scarf', 'gloves', 'socks', 'shoes',
-    'boots', 'sandals', 'sneakers', 'hat', 'tie', 'belt',
-    'glasses', 'watch', 'bracelet', 'earrings', 'ring'
-    ]
-
+    # Open the file to append the detected objects
+    file = open("dataset/train.txt", "a")
 
     # Function to perform object detection on a frame
     def perform_object_detection(frame):
+        # Move the frame to the CPU
+        frame = frame.cpu()
+
         # Convert the frame to a numpy array
         frame = frame.numpy()
 
@@ -70,17 +38,24 @@ def object():
         # Move predictions to CPU for further processing
         predictions = [{k: v.to(torch.device('cpu')) for k, v in pred.items()} for pred in predictions]
 
-        # Extract the bounding boxes, labels, and scores
-        boxes = predictions[0]['boxes'].numpy().astype(int)
-        labels = predictions[0]['labels'].numpy()
-        scores = predictions[0]['scores'].numpy()
+        # Write detected objects' IDs to the file
+        for pred in predictions:
+            labels = pred['labels'].numpy()
+            scores = pred['scores'].numpy()
+            boxes = pred['boxes'].numpy().astype(int)
+            for label, score, box in zip(labels, scores, boxes):
+                if score > 0.5:  # Set a threshold for the confidence score
+                    file.write(f"item {{\n  id: {label}\n}}\n")
 
-        # Draw bounding boxes and labels on the frame
-        for box, label, score in zip(boxes, labels, scores):
-            if score > 0.5:  # Set a threshold for the confidence score
-                x1, y1, x2, y2 = box
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.putText(frame, class_labels[label], (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+        # Draw bounding boxes on the frame
+        for pred in predictions:
+            labels = pred['labels'].numpy()
+            scores = pred['scores'].numpy()
+            boxes = pred['boxes'].numpy().astype(int)
+            for label, score, box in zip(labels, scores, boxes):
+                if score > 0.5:  # Set a threshold for the confidence score
+                    x1, y1, x2, y2 = box
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
         return frame
 
@@ -110,5 +85,8 @@ def object():
     cap.release()
     cv2.destroyAllWindows()
 
-# Call the satellite function
+    # Close the file
+    file.close()
+
+# Call the object function
 object()
