befa78799e1c7fc8d192112bad5306dc833de619
swayampakula.v.s.s.pavanakasinadha sarma <81065703+kasinadhsarma@users.noreply.github.com>
2023-06-09 23:00:11 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,27 +7,12 @@ from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
 import os
 import numpy as np
 import threading
-import time
 
 gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk, Gdk, GdkPixbuf, GLib
-COCO_CLASSES = [
-    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
-    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
-    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
-    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
-    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
-    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
-    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
-    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
-    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
-    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush', 'rikshaw'
-]
 
 def perform_object_detection(model, device, frame):
-    frame = torch.from_numpy(frame).permute(2, 0, 1).float() / 255.0
+    frame = torch.from_numpy(frame).permute(2, 0, 1).float() /255.0
     frame = frame.to(device)
 
     with torch.no_grad():
@@ -36,7 +21,6 @@ def perform_object_detection(model, device, frame):
     predictions = [{k: v.to(torch.device('cpu')) for k, v in pred.items()} for pred in predictions]
 
     frame_with_objects = frame.permute(1, 2, 0).mul(255).byte().numpy()
-    num_objects = 0
     for pred in predictions:
         labels = pred['labels'].numpy()
         scores = pred['scores'].numpy()
@@ -44,13 +28,10 @@ def perform_object_detection(model, device, frame):
         for label, score, box in zip(labels, scores, boxes):
             if score > 0.5:
                 x1, y1, x2, y2 = box
-                cv2.rectangle(frame_with_objects, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                class_name = COCO_CLASSES[label]
-                cv2.putText(frame_with_objects, class_name, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                num_objects += 1
-    print(f"Detected {num_objects} objects")
+                cv2.rectangle(frame_with_objects, (1, y1), (x2, y2), (0, 255,0), 2)
     return frame_with_objects, predictions
-def object_detection_setup():
+
+def object_detection():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=False, num_classes=91, pretrained_backbone=True)
@@ -77,24 +58,10 @@ class FrameCaptureThread(threading.Thread):
     def stop(self):
         self.running = False
 
-class UpdateFrameThread(threading.Thread):
-    def __init__(self, window):
-        super().__init__()
-        self.window = window
-        self.running = True
-
-    def run(self):
-        while self.running:
-            self.window.update_frame()
-            time.sleep(0.01)  # Sleep for 10 milliseconds
-
-    def stop(self):
-        self.running = False
-
 class ObjectDetectionWindow(Gtk.ApplicationWindow):
     def __init__(self, application):
         super().__init__(application=application)
-        self.set_title("DeepActionsExperimental")
+        self.set_title("Satellite-Oriented Object Detection")
         self.set_default_size(640, 480)
 
         self.model, self.device = object_detection_setup()
@@ -102,21 +69,16 @@ class ObjectDetectionWindow(Gtk.ApplicationWindow):
         self.image = Gtk.Image()
         self.set_child(self.image)
 
-        self.update_frame_thread = UpdateFrameThread(self)
-        self.update_frame_thread.start()
+        self.timeout_id = GLib.timeout_add(30, self.update_frame)
 
     def update_frame(self):
         frame = self.capture_thread.frame
         if frame is None:
             return True
 
-        print(f"Frame shape: {frame.shape}")
-
-        result = perform_object_detection(self.model, self.device, frame)
-        if result is not None:
-            frame_with_objects, _ = result
-            pixbuf = self.numpy_to_pixbuf(frame_with_objects)
-            self.image.set_from_pixbuf(pixbuf)
+        frame_with_objects, _ = perform_object_detection(self.model, self.device, frame)
+        pixbuf = self.numpy_to_pixbuf(frame_with_objects)
+        self.image.set_from_pixbuf(pixbuf)
 
         return True
 
@@ -134,8 +96,10 @@ class ObjectDetectionWindow(Gtk.ApplicationWindow):
         )
 
     def do_destroy(self):
-        self.update_frame_thread.stop()
-        self.update_frame_thread.join()
+        GLib.source_remove(self.timeout_id)
+
+    def do_show(self):
+        Gtk.Widget.do_show_all(self)
 
 class ObjectDetectionApp(Gtk.Application):
     def __init__(self):
@@ -156,7 +120,7 @@ class ObjectDetectionApp(Gtk.Application):
         win = ObjectDetectionWindow(self)
         win.capture_thread = self.capture_thread
         win.connect("destroy", self.on_destroy)
-        win.present()
+        win.show()
 
     def on_destroy(self, window):
         window.capture_thread.stop()
